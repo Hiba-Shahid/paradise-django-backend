4be69b724104617c54171056f05ca8d8c83d4a31
Hiba-Shahid <hibashahid678@gmail.com>
2025-06-28 19:25:10 +0500

    Register Competition, Ticket, Winner, and Payment models in the admin interface; update Giftshop admin registration.

--- competition/admin.py
+++ competition/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Competition, Ticket, Winner, Payment
 
-# Register your models here.
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'prize_name', 'price_per_entry', 'total_tickets', 'remaining_tickets', 'status', 'start_date', 'end_date')
+    list_filter = ('status',)
+    search_fields = ('title', 'prize_name')
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('ticket_number', 'user', 'competition', 'issued_at')
+    search_fields = ('ticket_number', 'user__user__username')
+
+@admin.register(Winner)
+class WinnerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'competition', 'prize', 'win_date', 'prize_status')
+    list_filter = ('prize_status',)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'competition', 'amount', 'payment_method', 'status', 'timestamp')
+    list_filter = ('payment_method', 'status')
+    search_fields = ('user__user__username', 'transaction_id')
--- giftshop/admin.py
+++ giftshop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Collection, Order, OrderItem, Cart, CartItem
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Collection)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
